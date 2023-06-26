1ea26407c5d77ec03851e4e9bc0b403dc38dfe7d
Felipe <felipecerquin@hotmail.com>
2023-06-26 19:31:58 -0300

    Base de los invocadores

---
+++ AppInvocador/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppinvocadorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppInvocador'
---
+++ InstaBlog/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+def lobby(request):
+    return HttpResponse("<h1>Bienvenidos a InstaBlog<h1>")
+def perfil(request):
+    return HttpResponse("<h1>Hola petones<h1>")
+
+def inicioInvocador(self):
+    miHtml = open("C:/Users/Usuario/Desktop/Instablog/InstaBlog/InstaBlog/plantillas/template1.html")
+    plantilla = Template(miHtml.read())
+    miHtml.close()
+    miContext = Context()
+    documento = plantilla.render(miContext)
+    return HttpResponse (documento)
