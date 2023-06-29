924c20498f5d86eae9e12247f93d2c8f6babf3ec
Felipe <felipecerquin@hotmail.com>
2023-06-28 22:31:29 -0300

    herencias terminadas

--- AppInvocador/models.py
+++ AppInvocador/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class invocador(models.Model):
+    nombre = models.CharField(max_length=30)
+    nick = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"nombre: {self.nombre} - nick: {self.nick} - email: {self.email} "
+
+
+class coach(models.Model):
+    nombre = models.CharField(max_length=30)
+    nick = models.CharField(max_length=30)
+    email = models.EmailField()
+    equipo = models.CharField(max_length=30)
+
+
+class partida(models.Model):
+    tipo = models.CharField(max_length=30)
+    fechaJugada = models.DateField()
+    entregada = models.BooleanField()
