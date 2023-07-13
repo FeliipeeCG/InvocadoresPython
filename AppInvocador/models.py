1663831ec24fa14bdf53247d76a600b377ea6988
Felipe <felipecerquin@hotmail.com>
2023-07-12 21:37:52 -0300

    actualizaciones

--- AppInvocador/models.py
+++ AppInvocador/models.py
@@ -18,8 +18,14 @@ class coach(models.Model):
     email = models.EmailField()
     equipo = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f"nombre: {self.nombre} - nick: {self.nick} - equipo: {self.equipo} "
+
 
 class partida(models.Model):
     tipo = models.CharField(max_length=30)
     fechaJugada = models.DateField()
     entregada = models.BooleanField()
+
+    def __str__(self):
+        return f"Tipo: {self.tipo} - Fecha: {self.fechaJugada}  "
